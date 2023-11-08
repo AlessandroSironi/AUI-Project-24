f8c1596f9d1832e2170af1cb5da125af4c196ece
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-11-08 12:48:47 +0100

    OpenAI connection

--- express-backend/index.ts
+++ express-backend/index.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 
 // Load environment variables
 dotenv.config();
@@ -12,9 +11,12 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello from the server');
 });
 
-import testRouter from './src/routes/openaiRoute';
-app.use('/api/openaiRoute', testRouter);
+import testRouter from './src/routes/testRoute';
+app.use('/api/testController', testRouter);
+
+import openaiRouter from './src/routes/openaiRoute';
+app.use('/api/openaiController', openaiRouter);
 
 app.listen(port, () => {
-    console.log(`now listening on port ${port}`);
+    console.log(`Now listening on port ${port}`);
 });
--- express-backend/src/controllers/openaiController.ts
+++ express-backend/src/controllers/openaiController.ts
@@ -3,28 +3,47 @@ import {Request, Response} from 'express';
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
 const openaiController = async (req: Request, res: Response) => {
-  // Replace with your Azure OpenAI key
-  const key = "a4f7ac34a6ea4b528b92cdbe17b446d5";
-  const endpoint = "https://aui-openai.openai.azure.com/";
-  const client = new OpenAIClient(endpoint, new AzureKeyCredential(key));
+  try {
+    // Replace with your Azure OpenAI key
+    const key = 'a4f7ac34a6ea4b528b92cdbe17b446d5';
+    const endpoint = "https://aui-openai.openai.azure.com/";
+    //const endpoint = "https://aui-openai.openai.azure.com/openai/deployments/ChatGPT35/chat/completions?api-version=2023-07-01-preview"
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(key));
 
-  const examplePrompts = [
-    "How are you today?",
-    "What is Azure OpenAI?",
-    "Why do children love dinosaurs?",
-    "Generate a proof of Euler's identity",
-    "Describe in single words only the good things that come into your mind about your mother.",
-  ];
 
-  const deploymentName = "text-davinci-003";
+  /*   const examplePrompts = [
+      "How are you today?",
+      "What is Azure OpenAI?",
+      "Why do children love dinosaurs?",
+      "Generate a proof of Euler's identity",
+      "Describe in single words only the good things that come into your mind about your mother.",
+    ];
 
-  let promptIndex = 0;
-  const { choices } = await client.getCompletions(deploymentName, examplePrompts);
-  for (const choice of choices) {
-    const completion = choice.text;
-    console.log(`Input: ${examplePrompts[promptIndex++]}`);
+    const deploymentName = "text-davinci-003";
+
+    let promptIndex = 0;
+    const { choices } = await client.getCompletions(deploymentName, examplePrompts);
+    for (const choice of choices) {
+      const completion = choice.text;
+      console.log(`Input: ${examplePrompts[promptIndex++]}`);
+      console.log(`Chatbot: ${completion}`);
+    } */
+
+    //Generate a single completion
+    const examplePrompt = "Once upon a time";
+    const deploymentName = "ChatGPT35";
+    const version = "2023-07-01-preview";
+    const { choices } = await client.getCompletions(deploymentName, examplePrompt, { apiVersion: version });
+    const completion = choices[0].text;
+    console.log(`Input: ${examplePrompt}`);
     console.log(`Chatbot: ${completion}`);
-  }
+    res.send(`Input: ${completion}`);
+  } catch (error) {
+    console.error(error);
+  } 
+
+  //res.send('Hello from the openAI controller');
+  
 };
 
 export default openaiController;
