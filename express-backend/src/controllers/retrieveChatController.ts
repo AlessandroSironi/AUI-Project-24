49f66448d99c208302c161d27b7fd38960c2bb09
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-11-22 10:55:35 +0100

    Created global variables file

--- express-backend/src/controllers/retrieveChatController.ts
+++ express-backend/src/controllers/retrieveChatController.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import { env } from 'process';
 import {createClient} from '@supabase/supabase-js'
 import "../types/schema";
+import { prompt } from '../globalVariables';
 
 const supabaseUrl = env.SUPABASE_PROJECT ?? "default_url";
 const supabaseKey = env.SUPABASE_KEY ?? "default_key";
@@ -9,11 +10,6 @@ const supabaseKey = env.SUPABASE_KEY ?? "default_key";
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
 const MESSAGE_LIMIT = 25;
-
-const prompt = {
-  "role":"system",
-  "content":"You are GreenBot. The user's name is Siro. As a highly-intelligent AI, you provide guidance on green energy practices, energy consumption optimization, and cultivating environmentally friendly habits. You possess JSON files containing information about your appliances and their energy consumption, and you aim to give advice and potentially generate IFTTT routines for energy management. Your responsive should be concise and straight to the point. You are not allowed to talk about anything else."
-}
 
 const retrieveChatController = async (req: Request, res: Response) => {
   try {
---
+++ express-backend/src/globalVariables.ts
@@ -0,0 +1,9 @@
+/**
+ * This file is used to hold all global variables.
+ */
+
+
+export const prompt = {
+    "role":"system",
+    "content":"You are GreenBot. The user's name is Siro. As a highly-intelligent AI, you provide guidance on green energy practices, energy consumption optimization, and cultivating environmentally friendly habits. You possess JSON files containing information about your appliances and their energy consumption, and you aim to give advice and potentially generate IFTTT routines for energy management. Your responsive should be concise and straight to the point. You are not allowed to talk about anything else."
+  }
