9d3761d2a342eecb083e252f9ea2c6709384cf44
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-11-22 11:55:18 +0100

    Update openaiController.ts

--- express-backend/src/controllers/openaiController.ts
+++ express-backend/src/controllers/openaiController.ts
@@ -1,14 +1,10 @@
 import express, {Request, Response} from 'express';
 import { env } from 'process';
 import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
-<<<<<<< HEAD
-=======
 import {createClient} from '@supabase/supabase-js'
 
 
->>>>>>> d621c8c6bff693e6fb8394e35395ed1b2cf02e2f
 import "../types/schema";
-import { prompt } from '../globalVariables';
 
 const key = env.OPENAI_KEY ?? "default_key";
 const endpoint = "https://aui-openai.openai.azure.com/";
@@ -16,8 +12,6 @@ const client = new OpenAIClient(endpoint, new AzureKeyCredential(key));
 const deploymentName = "ChatGPT35";
 const version = "2023-07-01-preview";
 
-<<<<<<< HEAD
-=======
 const supabaseUrl = env.SUPABASE_PROJECT ?? "default_url";
 const supabaseKey = env.SUPABASE_KEY ?? "default_key";
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
@@ -27,7 +21,6 @@ const prompt = {
   "content":"You are GreenBot. The user's name is Siro. As a highly-intelligent AI, you provide guidance on green energy practices, energy consumption optimization, and cultivating environmentally friendly habits. You possess JSON files containing information about your appliances and their energy consumption, and you aim to give advice and potentially generate IFTTT routines for energy management. Your responsive should be concise and straight to the point. You are not allowed to talk about anything else."
 }
 
->>>>>>> d621c8c6bff693e6fb8394e35395ed1b2cf02e2f
 const lightuserQuestion = "Provide me some examples on how to save energy.";
 const poweruserQuestion = "I want to create an IFTTT routine. Ask me what for and then explain it to me. Then generate complete IFTTT JSON code that sets up the routine. Before the JSON, please write CODE .";
 
@@ -131,4 +124,3 @@ const openaiController = async (req: Request, res: Response) => { //TODO: async?
 };
 
 export default openaiController;
-
