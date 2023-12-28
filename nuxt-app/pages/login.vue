b1ab3e977c05fa0677761855c524c643b8a3a405
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-12-28 12:37:43 +0100

    Login and signup styled

--- nuxt-app/pages/login.vue
+++ nuxt-app/pages/login.vue
@@ -28,13 +28,88 @@ const signInWithPassword = async () => {
 };
 </script>
 <template>
-    <div>
-        <h1>Login</h1>
-        <input v-model="email" type="email" placeholder="Your email" /><br />
-        <input v-model="password" type="password" placeholder="Your password" /><br />
-        <button @click="signInWithPassword"> Sign In with E-Mail </button><br />
-        <p>or you can <NuxtLink to="/signup">Register here</NuxtLink></p>
+    <div class="body-container">
+        <div class="form-container">
+            <h1 class="title">Login</h1>
+            <input v-model="email" type="email" placeholder="Your email" /><br />
+            <input v-model="password" type="password" placeholder="Your password" /><br />
+            <button class="button" @click="signInWithPassword"> Sign In with E-Mail </button><br />
+            <p class="subtext">or you can <NuxtLink class="button-2" to="/signup">Register here</NuxtLink></p>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+/* Style the body to center the form container */
+.body-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0; /* Light grey background */
+    font-family: Arial, sans-serif; /* Optional: Set a default font */
+}
+
+/* Style the container for the form */
+.form-container {
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px; /* Rounded corners */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Optional: Add a subtle shadow */
+    width: 300px; /* Fixed width, adjust as needed */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.title {
+    margin: 0;
+    margin-bottom: 10px;
+}
+
+/* Style the form elements */
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.subtext {
+    margin: 0;
+    margin-top: 10px;
+    font-size: 0.9rem;
+}
+
+input[type="email"],
+input[type="password"] {
+    padding: 10px;
+    margin-bottom: 5px;
+    border: 1px solid #ddd; /* Light grey border */
+    border-radius: 5px; /* Slightly rounded corners for input fields */
+    font-size: 0.9rem; /* Increase font size */
+    width: 100%; /* Full width */
+}
+
+.button {
+    padding: 10px;
+    background-color: var(--green-2);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    width: 100%; /* Full width */
+    font-size: 0.9rem;
+}
+
+.button-2 {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    background-color: var(--white-2);
+    color: var(--black-1);
+}
+
+</style>
